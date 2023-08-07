75792c43dd675ce8e6b2152d692d00aed501c2bd
moroz69off <moroz69off@gmail.com>
2023-08-07 08:19:17 +0300

    delete old

--- MainForm.cs
+++ MainForm.cs
@@ -17,15 +17,7 @@ namespace YouDl
 	{
 		string[] queries;
 		byte[] videoBytes;
-		List<byte[]> videosData = new List<byte[]>();
-		List<string> videoNames = new List<string>();
-		string fileName;
 		char[] delimiter = new char[] { ',' };
-
-		MessageBoxButtons buttons = MessageBoxButtons.YesNo;
-		MessageBoxIcon icon = MessageBoxIcon.Question;
-		string message = "Safe this video?";
-		string caption = "Safe question";
 
 		public MainForm()
 		{
@@ -58,7 +50,6 @@ namespace YouDl
 					{
 						output.Write(buffer, 0, read);
 						totalRead += read;
-						Console.Write($"\rDownloading {totalRead}/{totalByte} ...");
 					}
 				}
 			}
@@ -68,60 +59,6 @@ namespace YouDl
 		{
             Task result = MTask(queries);
 			result.Wait(36000);
-		}
-
-
-        /// <summary>
-        /// Initiates a client for every video address
-        /// </summary>
-        /// <param name="queries">Several (or one) youtube video addresses separated by a comma - «,»</param>
-  //      private void VideoLib(string[] queries)
-		//{
-		//	using (var cli = Client.For(YouTube.Default))
-		//	{
-		//		for (int i = 0; i < queries.Length; i++)
-		//		{
-		//			try
-		//			{
-		//				var video = cli.GetVideo(queries[i]);
-		//				videoBytes = video.GetBytes();
-		//				videosData.Add(videoBytes);
-		//				videoNames.Add(video.FullName);
-		//				ViewResult(video, i);
-		//			}
-		//			catch (Exception ex)
-		//			{
-		//				MessageBox.Show(ex.Message, ex.Source);
-		//			}
-		//		}
-		//		DialogResult result = MessageBox.Show(this, message, caption, buttons, icon);
-		//		if (result == DialogResult.Yes)
-		//		{
-		//			ButtonSave_Click(result, null);
-		//		}
-		//	}
-		//}
-
-		private void ViewResult(YouTubeVideo video, int i)
-		{
-			result_textBox.Text +=
-				$"found video {i+1} from the you list «{video.Title}»\r\n";
-		}
-
-		private void ButtonSave_Click(object sender, EventArgs e)
-		{
-            for (int i = 0; i < videoNames.Count; i++)
-            {
-				saveFileDialog.FileName = videoNames[i];
-				videoBytes = videosData[i];
-				saveFileDialog.ShowDialog();
-			}
-		}
-
-		private void SaveFileDialog_FileOk(object sender, CancelEventArgs e)
-		{
-			saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
-			File.WriteAllBytes(saveFileDialog.FileName, videoBytes);
 		}
 	}
 }
