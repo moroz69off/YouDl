d43d1fdfd7f13ade30939504bfac31e463110ba6
moroz69off <moroz69off@gmail.com>
2023-08-07 07:41:28 +0300

    filename

--- MainForm.cs
+++ MainForm.cs
@@ -37,15 +37,13 @@ namespace YouDl
 			queries = input_textBox.Text.Split(delimiter).ToArray();
 		}
 
-
-		//======================================================
 		async Task MTask(string[] args)
 		{
 			var youtube = YouTube.Default;
 			var video = youtube.GetVideo(queries[0]);
 			var client = new HttpClient();
 			long? totalByte = 0;
-			using (Stream output = File.OpenWrite(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + video.Title))
+			using (Stream output = File.OpenWrite(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "//" + video.Title + ".mp4"))
 			{
 				using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
 				{
@@ -56,29 +54,20 @@ namespace YouDl
 					byte[] buffer = new byte[16 * 1024];
 					int read;
 					int totalRead = 0;
-					Console.WriteLine("Download Started");
 					while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
 					{
 						output.Write(buffer, 0, read);
 						totalRead += read;
 						Console.Write($"\rDownloading {totalRead}/{totalByte} ...");
 					}
-					Console.WriteLine("Download Complete");
 				}
 			}
 		}
-		//======================================================
-
 
 		private void ButtonGo_Click(object sender, EventArgs e)
 		{
-			// VideoLib(queries);
-            //task = Task.Run(() => {
-            //	MessageBox.Show("task run");
-            //});
-            //task.Wait();
             Task result = MTask(queries);
-			result.Wait(20000);
+			result.Wait(36000);
 		}
 
 
@@ -86,32 +75,32 @@ namespace YouDl
         /// Initiates a client for every video address
         /// </summary>
         /// <param name="queries">Several (or one) youtube video addresses separated by a comma - «,»</param>
-        private void VideoLib(string[] queries)
-		{
-			using (var cli = Client.For(YouTube.Default))
-			{
-				for (int i = 0; i < queries.Length; i++)
-				{
-					try
-					{
-						var video = cli.GetVideo(queries[i]);
-						videoBytes = video.GetBytes();
-						videosData.Add(videoBytes);
-						videoNames.Add(video.FullName);
-						ViewResult(video, i);
-					}
-					catch (Exception ex)
-					{
-						MessageBox.Show(ex.Message, ex.Source);
-					}
-				}
-				DialogResult result = MessageBox.Show(this, message, caption, buttons, icon);
-				if (result == DialogResult.Yes)
-				{
-					ButtonSave_Click(result, null);
-				}
-			}
-		}
+  //      private void VideoLib(string[] queries)
+		//{
+		//	using (var cli = Client.For(YouTube.Default))
+		//	{
+		//		for (int i = 0; i < queries.Length; i++)
+		//		{
+		//			try
+		//			{
+		//				var video = cli.GetVideo(queries[i]);
+		//				videoBytes = video.GetBytes();
+		//				videosData.Add(videoBytes);
+		//				videoNames.Add(video.FullName);
+		//				ViewResult(video, i);
+		//			}
+		//			catch (Exception ex)
+		//			{
+		//				MessageBox.Show(ex.Message, ex.Source);
+		//			}
+		//		}
+		//		DialogResult result = MessageBox.Show(this, message, caption, buttons, icon);
+		//		if (result == DialogResult.Yes)
+		//		{
+		//			ButtonSave_Click(result, null);
+		//		}
+		//	}
+		//}
 
 		private void ViewResult(YouTubeVideo video, int i)
 		{
