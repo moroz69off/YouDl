e6b534fa0d6ef6ffb880a3511740ccf2f1e53ddd
moroz69off <moroz69off@gmail.com>
2023-11-30 22:08:18 +0300

    many videos to cycle

--- MainForm.cs
+++ MainForm.cs
@@ -17,7 +17,7 @@ namespace YouDl
     public partial class MainForm : Form
     {
         string[] queries;
-        byte[] videoBytes;
+        //byte[] videoBytes;
         char[] delimiter = new char[] { ',' };
 
         public MainForm()
@@ -33,36 +33,41 @@ namespace YouDl
         async Task MTask(string[] args)
         {
             YouTube youtube = YouTube.Default;
-            YouTubeVideo video = youtube.GetVideo(queries[0]);
-            string title = video.Title;
-            title = title.Replace('/', '_').Replace('"', '_');
+            //YouTubeVideo video = youtube.GetVideo(queries[0]);
+            //string title = video.Title.Replace('/', '_').Replace('"', '_');
+            
             IEnumerable<YouTubeVideo> videos = youtube.GetAllVideos(queries[0]);
-            var client = new HttpClient();
-            long? totalByte = 0;
-            using (Stream output = File.OpenWrite(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "//" + title + ".mp4"))
-            {
-                using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
-                {
-                    totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
-                }
-                using (var input = await client.GetStreamAsync(video.Uri))
-                {
-                    byte[] buffer = new byte[16 * 1024];
-                    int read;
-                    int totalRead = 0;
-                    while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
-                    {
-                        output.Write(buffer, 0, read);
-                        totalRead += read;
-                    }
-                }
-            }
+
+			for(int i = 0; i < queries.Length; i++){
+				YouTubeVideo video = youtube.GetVideo(queries[i]);
+                string title = video.Title.Replace('/', '_').Replace('"', '_');
+                var client = new HttpClient();
+				long? totalByte = 0;
+				using (Stream output = File.OpenWrite(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "//" + title + ".mp4"))
+				{
+					using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
+					{
+						totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
+					}
+					using (var input = await client.GetStreamAsync(video.Uri))
+					{
+						byte[] buffer = new byte[16 * 1024];
+						int read;
+						int totalRead = 0;
+						while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+						{
+							output.Write(buffer, 0, read);
+							totalRead += read;
+						}
+					}
+				}
+			}
         }
 
         private void ButtonGo_Click(object sender, EventArgs e)
         {
             Task result = MTask(queries);
-            result.Wait(100);
+            result.Wait(1969);
         }
     }
 }
