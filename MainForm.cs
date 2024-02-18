a71570763d89f003b0d3e2fc8f784c354b5472a5
moroz69off <moroz69off@gmail.com>
2024-02-18 08:27:30 +0300

    bad title repair

--- MainForm.cs
+++ MainForm.cs
@@ -18,7 +18,8 @@ namespace YouDl
     {
         string[] queries;
         //byte[] videoBytes;
-        char[] delimiter = new char[] { ',' };
+        char[] delimiter = new char[] { ',', '/', '"', '*', '•', '¹', '©', '—', '`', '~' };
+        string title;
 
         public MainForm()
         {
@@ -38,30 +39,39 @@ namespace YouDl
             
             IEnumerable<YouTubeVideo> videos = youtube.GetAllVideos(queries[0]);
 
-			for(int i = 0; i < queries.Length; i++){
-				YouTubeVideo video = youtube.GetVideo(queries[i]);
-                string title = video.Title.Replace('/', '_').Replace('"', '_').Replace('*', '_');
+			for(int i = 0; i < queries.Length; i++)
+            {
+                YouTubeVideo video = youtube.GetVideo(queries[i]);
+                BadTitleRepair(video);
                 var client = new HttpClient();
-				long? totalByte = 0;
-				using (Stream output = File.OpenWrite(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "//" + title + ".mp4"))
-				{
-					using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
-					{
-						totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
-					}
-					using (var input = await client.GetStreamAsync(video.Uri))
-					{
-						byte[] buffer = new byte[16 * 1024];
-						int read;
-						int totalRead = 0;
-						while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
-						{
-							output.Write(buffer, 0, read);
-							totalRead += read;
-						}
-					}
-				}
-			}
+                long? totalByte = 0;
+                using (Stream output = File.OpenWrite(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "//" + title + ".mp4"))
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
+                    {
+                        totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
+                    }
+                    using (var input = await client.GetStreamAsync(video.Uri))
+                    {
+                        byte[] buffer = new byte[16 * 1024];
+                        int read;
+                        int totalRead = 0;
+                        while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            output.Write(buffer, 0, read);
+                            totalRead += read;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void BadTitleRepair(YouTubeVideo video)
+        {
+            for (int j = 0; j < delimiter.Length; j++)
+            {
+                title = video.Title.Replace(delimiter[j], '_');
+            }
         }
 
         private void ButtonGo_Click(object sender, EventArgs e)
