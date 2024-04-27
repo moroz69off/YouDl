8648b9eeacbd4acae2fbc3cb06c1d1141162df22
moroz69off <moroz69off@gmail.com>
2024-04-27 23:36:24 +0300

    exception

--- MainForm.cs
+++ MainForm.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -35,39 +35,42 @@ namespace YouDl
             YouTube youtube = YouTube.Default;
             //YouTubeVideo video = youtube.GetVideo(queries[0]);
             //string title = video.Title.Replace('/', '_').Replace('"', '_');
-            
+
             // IEnumerable<YouTubeVideo> videos = youtube.GetAllVideos(queries[0]);
 
-			for(int i = 0; i < queries.Length; i++){
-				YouTubeVideo video = youtube.GetVideo(queries[i]);
-                string title = video.Title.Replace('/', '_').Replace('"', '_').Replace('*', '_');
+            for (int i = 0; i < queries.Length; i++)
+            {
+                YouTubeVideo video = youtube.GetVideo(queries[i]);
+                string title = video.Title.Replace('/', '_').Replace('"', '_').Replace('*', '_').Replace('#', '_');
+                //�
                 var client = new HttpClient();
-				long? totalByte;
-				using (Stream output = File.OpenWrite(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "//" + title + ".mp4"))
-				{
-					using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
-					{
-						totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
-					}
-					using (var input = await client.GetStreamAsync(video.Uri))
-					{
-						byte[] buffer = new byte[16 * 1024];
-						int read;
-						int totalRead = 0;
-						while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
-						{
-							output.Write(buffer, 0, read);
-							totalRead += read;
-						}
-					}
-				}
-			}
+                long? totalByte;
+                using (Stream output = File.OpenWrite(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "//" + title + ".mp4"))
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Head, video.Uri))
+                    {
+                        totalByte = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result.Content.Headers.ContentLength;
+                    }
+                    using (var input = await client.GetStreamAsync(video.Uri))
+                    {
+                        byte[] buffer = new byte[16 * 1024];
+                        int read;
+                        int totalRead = 0;
+                        while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            output.Write(buffer, 0, read);
+                            totalRead += read;
+                        }
+                    }
+                }
+            }
         }
 
         private void ButtonGo_Click(object sender, EventArgs e)
         {
             Task result = MTask(queries);
-            result.Wait(1969);
+            result.Wait();
+
         }
     }
 }
